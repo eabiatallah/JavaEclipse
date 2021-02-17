b07644f05b90d5df35d47cafc04eca7a08ef20fd
EliasAbiAtallah <eabiatallah@gmail.com>
2021-02-17 18:17:38 -0500

    First Commit from Eclipse

---
+++ java8lambdabasics/src/io/javabrains/inAction/chp10/ex02.java
@@ -0,0 +1,16 @@
+package io.javabrains.inAction.chp10;
+
+import java.util.Optional;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		 
+		Optional<String> empty = Optional.empty();
+		System.out.println("empty = "+empty);
+		
+		
+
+	}
+
+}
