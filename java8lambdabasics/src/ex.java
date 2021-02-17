a29fac67dcaefb5eee19e1b73c9e2ae764953870
EliasAbiAtallah <eabiatallah@gmail.com>
2021-02-17 18:20:06 -0500

    First Commit from Eclipse

---
+++ java8lambdabasics/src/ex.java
@@ -0,0 +1,7 @@
+public class ex{    
+  public static void main(String args[]){
+          String x = "15.21";
+          Float f1 = Float.parseFloat(x);
+          System.out.println(f1);            
+  }
+}
